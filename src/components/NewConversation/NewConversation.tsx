f055ef06777639d46db0787b044db37fceb2df57
Max Anfilofyev <maksimint@gmail.com>
2024-04-16 03:08:37 -0400

    another attempt at fixing the location issue

--- src/components/NewConversation/NewConversation.tsx
+++ src/components/NewConversation/NewConversation.tsx
@@ -132,7 +132,7 @@ export default function NewConversation() {
         const uploadLocation = getUploadMetadata();
         const s3Location = {
             Bucket: uploadLocation.bucket,
-            Key: `${uploadLocation.key}/private/${(filePath as File).name}`,
+            Key: `private/${uploadLocation.key}/${(filePath as File).name}`,
         };
 
         const jobParams: StartMedicalScribeJobRequest = {
